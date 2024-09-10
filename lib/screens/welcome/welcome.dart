c55272aba9c4e7e4165ac1220d6785235bb5af94
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-10 16:42:41 -0400

    Added Sign In and Sign Up Form Templates

--- lib/screens/welcome/welcome.dart
+++ lib/screens/welcome/welcome.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_auth_tut/screens/welcome/sign_in.dart';
+import 'package:flutter_auth_tut/screens/welcome/sign_up.dart';
 import 'package:flutter_auth_tut/shared/styled_text.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +11,6 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,19 +21,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 48, horizontal: 16),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              StyledHeading('Welcome.'),
-        
-              // sign up screen
+            padding: const EdgeInsets.symmetric(vertical: 48, horizontal: 16),
+            child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  StyledHeading('Welcome.'),
 
+                  // sign up screen
+                  SignUpForm(),
 
-              // sign in screen
-            ]
-          )
-        ),
+                  // sign in screen
+                  SignInForm(),
+                ])),
       ),
     );
   }
